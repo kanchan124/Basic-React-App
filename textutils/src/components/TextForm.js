9328c65ec041d10e3fd10bc3dad415c854f33217
Kanchan Bawane <kanchan.bawane@perficient.com>
2022-10-23 21:59:58 +0530

    states in React

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const handleUppercaseClick = () => {
+    setText(text.toLocaleUpperCase())
+  };
+
+  const handleOnChnage = (event) => {
+    setText(event.target.value)
+  };
+
+  //state in react
+  const [text, setText] = useState("Enter Text Here");
+  /*   setText("updated text"); */
   return (
     <div>
-      <h1>{props.heading}</h1>
+      <h1>
+        {props.heading}-{text}
+      </h1>
       <div className="mb-3">
         <textarea
           className="form-control"
+          value={text}
           id="myform"
           rows="8"
+          onChange={handleOnChnage}
         ></textarea>
       </div>
-      <button className="btn btn-primary">Convert to uppercase</button>
+      <button className="btn btn-primary" onClick={handleUppercaseClick}>
+        Convert to uppercase
+      </button>
     </div>
   );
 }
